ed16bfa7d14a1b509891c55c935ec86664ace52b
Sidharth Nair <sidnair@utexas.edu>
2022-11-14 15:42:17 -0600

    done

--- testcases/C.java
+++ testcases/C.java
@@ -1,7 +1,8 @@
 
 public class C {
 	public static int m(int a, int b) { // a=10, b=22
-		int i = 3 iff a > b else 5; // result would be 5
+		int j = a > b ? 3 : 5;
+		int i = 3 iff a > b else 7 iff b > 23 else 9; // result would be 5
 		return i;
 	}
 	public static void main(String... args) {
